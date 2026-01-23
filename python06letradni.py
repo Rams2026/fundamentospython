f793a1d6b70b6c525fc6023e9983751a29645479
“Rams2026” <raul_a86@hotmail.com>
2026-01-23 18:08:27 +0100

    a

--- python06letradni.py
+++ python06letradni.py
@@ -1,7 +1,2 @@
-
-import datetime
-def obtener_dia_nacimiento():
-    #pedir fecha de nacimiento 
-    fecha_str = input("teclea fecha de nacimiento (dd/mm/aaaa): ")
-    print (fecha_str)
-    fecha_nac = datetime.datetime.strptime(fecha_str, "%d/%m/%Y")
+#obtener dia nacimiento
+print("obtener dia nacimiento")
