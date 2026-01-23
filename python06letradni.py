14905629597e7e49cd059d4ca5600af99c92dae3
“Rams2026” <raul_a86@hotmail.com>
2026-01-23 17:24:26 +0100

    a

---
+++ python06letradni.py
@@ -0,0 +1,7 @@
+print ("calcula la letra del DNI")
+import datetime
+def obtener_dia_nacimiento():
+    #pedir fecha de nacimiento 
+    fecha_str = input("teclea fecha de nacimiento (dd/mm/aaaa): ")
+    print (fecha_str)
+    fecha_nac = datetime.datetime.strptime(fecha_str, "%d/%m/%y")
---
+++ python07librerias.py
@@ -0,0 +1,8 @@
+print("Librerias python")
+import math
+resultado =(7 / 3)
+print(resultado)
+
+print("Floor", math.floor(resultado))
+
+print("Fin Pgm")
