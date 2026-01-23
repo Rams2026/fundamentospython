ee57cb3c2ee86c623636968dc00f2001ceb05aac
“Rams2026” <raul_a86@hotmail.com>
2026-01-23 18:06:39 +0100

    a

--- python08diasemana.py
+++ python08diasemana.py
@@ -1,4 +1,19 @@
 print("calcula ")
 
+print("Teclea el dia DD")
+DD = int(input())
+print("Teclea el numero de mes MM")
+MM = int(input())
+print("Teclea el año AAAA")
+AAAA = int(input())
+if(MM == 1):
+    MM = 13
+    AAAA = AAAA - 1
+elif(MM == 2)
+     MM
+
+
+
+
 
 
