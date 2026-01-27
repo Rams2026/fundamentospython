32b759f5061aacfa2f6bd0bdaf75128b680cf411
“Rams2026” <raul_a86@hotmail.com>
2026-01-27 19:17:16 +0100

    l

--- python20metodos.py
+++ python20metodos.py
@@ -3,6 +3,15 @@ def primerMetodo():
     print("Primer Metodo")
 def segundoMetodo():
     print("Segundo Metodo")
+def saludar(nombre):
+    print("Bienvenido a Python Mr/Ms " + nombre)
+def despedirse(nombre, dia):
+    print("Ha sido un placer hoy ")
 primerMetodo()
 primerMetodo()
 segundoMetodo()
+despedirse("alumno", "martes", nombre= dia=)
+
+
+
+    
---
+++ python21metodosreturn.py
@@ -0,0 +1,8 @@
+def convertirMayusculas(texto):
+    return texto.upper()
+def convertirMinusculas(texto):
+    return texto.lower()
+def concatenar(texto1, texto2):
+    resultado = texto1 + texto2
+    return resultado
+
