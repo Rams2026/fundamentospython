387299a237c0d52679593aee9016108a326730fd
“Rams2026” <raul_a86@hotmail.com>
2026-01-27 19:38:47 +0100

    f

--- python21metodosreturn.py
+++ python21metodosreturn.py
@@ -5,4 +5,30 @@ def convertirMinusculas(texto):
 def concatenar(texto1, texto2):
     resultado = texto1 + texto2
     return resultado
+def mostrarmenu():
+    print("Seleccione una opcion")
+    print("1.- Convertir a mayusculas")
+    print("2.- Convertir a minusculas")
+    print("3.- Concatenar textos")
+#PROGRAMA PRINCIPAL MAIN
+print ("Ejemplos metodos return")
+print("introduce un texto")
+valor= input()
+mostrarmenu()
+opcion =int(input())
+resultado =""
+if (opcion == 1):
+    resultado = convertirMayusculas(valor)
+elif (opcion == 2):
+    resultado = convertirMinusculas(valor)
+elif(opcion == 3):
+    print("Dame otro texto")
+    otro = input()
+    resultado = concatenar(valor, otro)
+print(resultado)
+
+print("FIN PGM")
+
+
+
 
